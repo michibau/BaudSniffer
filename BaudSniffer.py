cef97be51c9d3ef8d1d0dd2d0750099a6a96490c
Michael Baurschmid <michaelbaurschmid@gmail.com>
2023-01-31 21:39:37 +0100

    Spelling improvments and better sorting at the end. It also displays the (probably) best connection now.

--- BaudSniffer.py
+++ BaudSniffer.py
@@ -2,13 +2,14 @@ import serial
 import serial.serialutil
 import platform
 import os
+import time
 
 # TODO: Standalone Tool with Options as a next step
 # TODO: Database with tested devices
 
 ## Global Var
-globalSerialport = 'COM4' # For Linux: /dev/ttyS1 etc. ; For Windows: COM1 etc. 
-globaelTimeout = 10 #dont change! - Timeout for searial port
+globalSettingForSerialport = 'COM4' # For Linux: /dev/ttyS1 etc. ; For Windows: COM1 etc. 
+globalTimeoutInSec = 10 #dont change! - Timeout for searial port
 globalByteSizetoRead = 10
 
 connectionList = []
@@ -57,7 +58,7 @@ def openPort(baudSet: int,
     newStopbitSet = setStopbit(stopbitSet)  
 
     try:
-        ser = serial.Serial(globalSerialport, baudSet, newBytesizeSet, newParitySet, newStopbitSet, timeout=globaelTimeout)  # open serial port
+        ser = serial.Serial(globalSettingForSerialport, baudSet, newBytesizeSet, newParitySet, newStopbitSet, timeout=globalTimeoutInSec)  # open serial port
         dataSerial = ser.read(globalByteSizetoRead) 
         ser.close()             # close port
         return dataSerial
@@ -66,7 +67,7 @@ def openPort(baudSet: int,
 
 def setStopbit(stopBit: str):
 
-    if stopBit == "1":
+    if stopBit == "1": 
         return serial.STOPBITS_ONE
     elif stopBit == "2":
         return serial.STOPBITS_TWO
@@ -96,6 +97,7 @@ def setParity (parity: str):
         return serial.PARITY_ODD
     elif parity == "M":
         return serial.PARITY_MARK
+
     elif parity == "S":
         return serial.PARITY_SPACE
     else: 
@@ -111,8 +113,7 @@ def tryBaudrate(serialportParameter = str): # serial port parameter = 8N1, 7N1 a
 
         validConnectionOption = estimateConnectionQuality(bytesInText, serialData)
         if validConnectionOption == True:
-            strBaudrate = str(baudrate)
-            connectionList.append(strBaudrate + ' bps \t' + serialportParameter)
+            connectionList.append((serialportParameter, baudrate))
 
     
 
@@ -131,17 +132,17 @@ def sliceSerialportParameter (serialportParameterString: str):
 # This functions trys to perform the same task as a human would do when each connection would be checked manual.
 # Screen, Minicom etc. would present the data as UTF-8 encoded and this function use this property to check for the following criteria:
 #
-# "text.encode() == serialData" checks if serial data and UTF-8 encoded text are the same
-# "(not text)" returns True for empty text
+# "text.encode() == serialData" checks if serial data bytes and UTF-8 encoded text are the same
+# "(not text)" returns True for empty text, and needs be False in this application aka text is not empty
 # "text.isprintable()" returns False for special characters
 #
-# If all the point above are fulfilled the serial properties like baudrate etc. should be correct. 
+# If all the points above are fulfilled the serial properties like baudrate etc. should be correct. 
 ##
 def estimateConnectionQuality(text: str, 
                               serialData: bytes):
 
     if (text.encode() == serialData) == True and (not text) == False and text.isprintable() == True:  
-            print ('\n########### Connection Properly ok #############\n')
+            print ('\n########### Connection probably ok #############\n')
             return True
 
 def displayText (actualBaud: str, 
@@ -156,62 +157,88 @@ def displayText (actualBaud: str,
     print("\n")
 
 def displayBestConnection():
-    # TODO: Improve List sorting. 
-    connectionList.sort(reverse=True)
-    print('Result List: (higher bps = better)\n\nBaud\t\tSerial Connection Propertys\n')
-    for eachConnetion in connectionList:
-        print (eachConnetion)
 
-def pause():
+    counterDisplayConnection = 0 
+    counterSortingBaud = 0
+    bestBaudrate = 0
+    bestCombinationText = 0
 
-    input("\nPress the <ENTER> key to exit or continue...\n")
+    connectionList.sort()
+    print("\n\t Found the following Settings:\n" )
+    print("\n\t Serial Settings \t Baudrate\n" )
+    while counterDisplayConnection < len(connectionList):
+        print("\t", connectionList[counterDisplayConnection][0], "\t\t\t\t", connectionList[counterDisplayConnection][1], "bps")
+        counterDisplayConnection+=1
+    
+    while counterSortingBaud < len(connectionList):
+        if bestBaudrate < connectionList[counterSortingBaud][1]:
+            bestBaudrate = connectionList[counterSortingBaud][1]
+            bestCombinationText =  str(connectionList[counterSortingBaud][0]) + " " + str(connectionList[counterSortingBaud][1] + " bps")
+        counterSortingBaud+=1
+    print("\n\n\tBest Connection:", bestCombinationText)
+
+
+
+def stopProgram():
+
+    input("\nPress any key to exit ...\n")
+    exit
 
 def returnDuration ():
-    estiamtedTimeInMin = (len(baudRateList)*len(serialportSettingsList)*globaelTimeout) * 0.0166667 # 1s = 0.0166667 min
-    return round(estiamtedTimeInMin, 2)
+
+    estimatedTimeInMin = (len(baudRateList)*len(serialportSettingsList)*globalTimeoutInSec) * 0.0166667 # 1s = 0.0166667 min
+    return round(estimatedTimeInMin, 2)
 
 ##
 #
-# Checks if a Windows C drive is a avaidable under Linux and rise warning
+# Checks if a Windows C drive is a avaidable under Linux and rise warning for WSL (Windows-Subsystem for Linux)
 #
 ##
 def checkforWSL():
+
         if (platform.platform()).__contains__('Linux'):
             test = os.popen('ls /mnt/c').read()
             if test.__contains__('Windows'):
                 print("\n\n\n\t\tAttention: \n")
                 print("\t\tIf you try to run the tool on WSL (Windows-Subsystem for Linux) please adjust the COM Port Parameter and use Windows.")
                 print("\t\tThe script will fail at the moment for this constellation.")
-                pause()
+                stopProgram()
  
 ##
 #
-# Checks for the correct COM port under Windows
+# Checks for the correct serial port under Windows
 #
 ##
 def checkForWindows():
-        if (platform.platform()).__contains__('Windows') and globalSerialport.__contains__('COM') == False:
+
+        if (platform.platform()).__contains__('Windows') and globalSettingForSerialport.__contains__('COM') == False:
             print("\n\n\t\tPlease adjust the COM Port for Windows.")
-            pause()
+            stopProgram()
 
 ##
 #
-# Checks for the correct COM port under Linux
+# Checks for the correct serial port under Linux
 #
 ##
 def checkForLinux():
-        if (platform.platform()).__contains__('Linux') and globalSerialport.__contains__('COM') == True:
+
+        if (platform.platform()).__contains__('Linux') and globalSettingForSerialport.__contains__('COM') == True:
             print("\n\n\t\tPlease adjust the COM Port for Linux.")
-            pause()
+            stopProgram()
         
 def main():
+
+    srciptStartTime = time.time()
     checkforWSL()
     checkForWindows()
     checkForLinux()
-    print ("\nUsed Serial Port: \n", globalSerialport, "\n", flush=True)
+    print ("\nUsed Serial Port: \n", globalSettingForSerialport, "\n")
     print ("Estimated Time: ", returnDuration() , "min\n")
     for serialSetting in serialportSettingsList:
         tryBaudrate (serialSetting)
     displayBestConnection()    
-    pause ()
+    scriptEndTime = time.time()
+    print("\nRuntime: ", round((scriptEndTime-srciptStartTime) * 0.0166667, 2) , " min")  # 1s = 0.0166667 min
+    stopProgram ()
+
 main()
